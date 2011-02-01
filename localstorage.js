41124ad7a167c8c655227ae6f7321ad295a8c826
Jamie <jamie@jamie.(none)>
2011-02-01 19:08:31 +0000

    First commit

---
+++ localstorage.js
@@ -0,0 +1,21 @@
+/* localstorage.js by Jamie McElwain */
+var ls = {
+	set: function(name,value){
+		/* Checks if the value is an object, and parses it into JSON. */
+		if (typeof value == "object") {
+			value = JSON.stringify(value);
+		}
+		localStorage.setItem(name, value);
+	},
+	get: function(name){
+		var value = localStorage.getItem(name);
+		/* Checks if the value is JSON, and parses it into an object. */
+		if (value[0] == "{") {
+			value = JSON.parse(value);
+		}
+		return value;
+	},
+	delete: function(name){
+		return localStorage.removeItem(name);
+	}
+};
